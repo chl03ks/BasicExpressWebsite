dad5352edb24898bc4643d133142eb97ee4779b0
Daniel Ulises Garcia Verdin <gvsdan@users.noreply.github.com>
2015-12-15 10:50:11 -0600

    Revert "Mail Form"

--- routes/contact.js
+++ routes/contact.js
@@ -1,36 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
 
 /* GET contact page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
-});
-
-router.post('/send', function (req, res, next) {
-
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'udevelopd@gmail.com',
-            pass: 'm49wapdaniel'
-        },
-
-    });
-
-    var mailOptions = {
-        from: 'John Doe <johndoe@outlook.com>',
-        to: 'udevelopd@gmail.com',
-        subjetc: 'Website Contact',
-        text: 'You have a new submission with the following details....Name: ' +
-               req.body.name + ' Email:'+ req.body.email + ' Message:' + req.body.message,
-        html: '<h1> You have a new mail from:</h1> ' + req.body.name,
-    };
-
-    transporter.sendMail(mailOptions, function(err, info){
-        if (err) console.log(err);
-        res.redirect('/');
-    });
 });
 
 module.exports = router;
